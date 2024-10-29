b63652b1071d918eb560157bb6baafe68be4da29
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-10-29 13:22:19 +0100

    dribble layout

---
+++ src/Components/Heroscreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Heroscreen = () => {
+  return (
+    <div>Heroscreen</div>
+  )
+}
+
+export default Heroscreen
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heroscreen from "../Components/Heroscreen";
+
+const Home = () => {
+  return (
+    <div>
+      <Heroscreen />
+    </div>
+  );
+};
+
+export default Home;
