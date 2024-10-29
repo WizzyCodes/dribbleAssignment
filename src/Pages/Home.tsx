7f0eb68e121106e18567cd41fe8e8136f0219edf
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-10-29 19:07:00 +0100

    slider

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heroscreen from "../Components/Heroscreen";
+import Slider from "../Components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Heroscreen />
+      <Slider />
     </div>
   );
 };
